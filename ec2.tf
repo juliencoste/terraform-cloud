3a92646b37978727dea5c89e7ad9658267e28f42
juliencoste <64328595+juliencoste@users.noreply.github.com>
2021-11-19 14:50:11 +0100

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-central1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-047e03b8591f2d48a"
+  instance_type = "t2.micro"
+  key_name      = "SSH-test-jc"
+  tags = {
+    Name = "ec2-julien"
+  }
+}
